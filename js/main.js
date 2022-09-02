b483f05702ba5c67cd79c1dd2789c5f99f7e6dd0
Edison Lee <edisonlee@edisonlee55.com>
2022-09-01 20:40:58 -0400

    feat: add Sakana! Widget

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+function initSakanaWidget() {
+  new SakanaWidget({ controls: false }).setState({ i: 0.005 }).mount('#sakana-widget');
+}
+
 var copyrightYear = new Date().getFullYear();
 $("#copyrightText").text("\u00A9" + copyrightYear + " edisonlee55");
 
@@ -5,13 +9,13 @@ var pageUrl_string = window.location.href;
 var pageUrl = new URL(pageUrl_string);
 var pagePeople = pageUrl.searchParams.get("p");
 if (pagePeople == "kafuu") {
-    document.title = "香風明謙 Kafuu Edison";
-    $("#navName").text("香風明謙 Kafuu Edison");
-    $("#titleName").html("香風明謙<br>Kafuu Edison");
+  document.title = "香風明謙 Kafuu Edison";
+  $("#navName").text("香風明謙 Kafuu Edison");
+  $("#titleName").html("香風明謙<br>Kafuu Edison");
 } else if (pagePeople == "gabu") {
-    document.title = "小加百 Gabu Meow";
-    $("#navName").text("小加百 Gabu Meow");
-    $("#titleName").html("小加百<br>Gabu Meow");
+  document.title = "小加百 Gabu Meow";
+  $("#navName").text("小加百 Gabu Meow");
+  $("#titleName").html("小加百<br>Gabu Meow");
 }
 
 /*
